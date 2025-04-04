1bf78c0dfb92b4eedfb7f2f87c37602dad74184e
EUGENIA CATTELAN <ecattelanpedrazzini@gmail.com>
2025-04-03 23:04:37 -0300

    agrego funcion 2 del paquete

--- src/funciones_ej9/__init__.py
+++ src/funciones_ej9/__init__.py
@@ -1,8 +1,10 @@
-import clean_space
+import clean_space, conv_title
 
 def cleaner (clients):
     list=[]
     for data in clients:
-        name=clean_space(data)
+        name_cleaned=clean_space(data)
+        if name_cleaned: #si no es vacio
+            name_cleaned= conv_title (name_cleaned)
 
     return list
---
+++ src/funciones_ej9/conv_title.py
@@ -0,0 +1,6 @@
+def conv_title (name):
+    if name:
+        name=name.title()
+        return name
+    else:
+        return None
